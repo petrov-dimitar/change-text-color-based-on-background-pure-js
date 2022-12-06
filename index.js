82144aa04a4d7f07ea48a285d22fc658eb57cd43
Dimitar Petrov <dimitar.petrov@bulcode.com>
2022-12-06 12:16:03 +0200

    Code refactoring.

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 /** @format */
-
-const textToBackgroundMapper = {
-  red: "white",
-  green: "yellow",
-  blue: "black",
-};
-
-function changeTextColor(scrollPos) {
-  // Do something with the scroll position
-
-  var headerLinks = document.getElementsByClassName("link");
-  //   headerLinks.style.color = textToBackgroundMapper;
+function changeTextFromToColor(colorFrom) {
+  // Get and loop through each links.
+  for (let spanLink of document.getElementsByClassName("link")) {
+    let colorTo = null;
+    switch (colorFrom) {
+      case "red":
+        colorTo = "powderblue";
+        break;
+      case "blue":
+        colorTo = "black";
+        break;
+      case "green":
+        colorTo = "yellow";
+        break;
+      default:
+        colorTo = "white";
+    }
+    spanLink.style.color = colorTo;
+  }
 }
 
 document.addEventListener("scroll", (e) => {
+  // Get wrapper of all background-colored divs
   const content = document.getElementById("content");
+
+  //   Get current scroll position.
   lastKnownScrollPosition = window.scrollY;
 
+  //   Get all background-colored divs.
   const children = content.children;
 
   if (
@@ -29,16 +40,13 @@ document.addEventListener("scroll", (e) => {
   for (let item of Array.from(children).reverse()) {
     const rect = item.getBoundingClientRect();
     const childPosition = rect.top + lastKnownScrollPosition;
-    // console.log("childPosition", childPosition);
-
     if (lastKnownScrollPosition >= childPosition) {
       if (item.style.backgroundColor) {
         console.log("inside", item.style.backgroundColor);
+        changeTextFromToColor(item.style.backgroundColor);
       } else {
       }
       break;
     }
   }
-
-  changeTextColor(lastKnownScrollPosition);
 });
